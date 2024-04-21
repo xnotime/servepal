50f1724a9b77ea577ea4026cabda4b0d9aee71f8
K40N <161914158+K40N@users.noreply.github.com>
2024-04-21 16:50:43 -0500

    seems like comms work

--- servepal.ino
+++ servepal.ino
@@ -16,8 +16,13 @@ unsigned long delaytime=5;
 const int FAN_PWM_PIN = 6;
 
 void setup() {
+  // serial stuffs
+  Serial.begin(115200);
+  while (!Serial) {}
+
   // fan pwm
   pinMode(FAN_PWM_PIN, OUTPUT);
+  analogWrite(FAN_PWM_PIN, 0);
 
   /*
    The MAX72XX is in power-saving mode on startup,
@@ -31,5 +36,51 @@ void setup() {
 }
 
 void loop() {
-  analogWrite(FAN_PWM_PIN, 0);
+  bool expectCommand = true;
+  char commandName = 0;
+  char commandArg = 0;
+  while (Serial.available() > 0) {
+    char c = Serial.read();
+    Serial.print("[ser] ");
+    Serial.print(c);
+    if (c == '\n') {
+      Serial.println(" -> newline");
+      if (!expectCommand) {
+        enactCommand(commandName, commandArg);
+      }
+      expectCommand = true;
+    } else if (expectCommand) {
+      Serial.println(" -> name");
+      commandName = c;
+      expectCommand = false;
+    } else {
+      Serial.println(" -> arg");
+      commandArg = c;
+    }
+  }
+}
+
+int currentRow = 0;
+
+void enactCommand(char name, char arg) {
+  Serial.print("[cmd] ");
+  Serial.print(name);
+  Serial.println((int)arg);
+  switch (name) {
+    case 'f':
+      analogWrite(FAN_PWM_PIN, (int)arg);
+      return;
+    case 'r':
+      currentRow = (int)arg;
+      return;
+    case 'd':
+      lc.setRow(0, currentRow, arg);
+      return;
+    case 'c':
+      lc.clearDisplay(0);
+      return;
+    case 'i':
+      lc.setIntensity(0, (int)arg);
+      return;
+  }
 }
