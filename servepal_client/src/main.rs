a3c016ff11d37789ba07f84b142f8bb91abb32ee
K40N <161914158+K40N@users.noreply.github.com>
2024-04-21 22:52:16 -0500

    basic client that does something

--- servepal_client/src/main.rs
+++ servepal_client/src/main.rs
@@ -1,13 +1,21 @@
-use std::time::Duration;
+use std::{thread, time::Duration};
 
 use serialport;
 
 const PORT_PATH: &'static str = "/dev/ttyUSB0";
+const BAUD: u32 = 115_200;
 
 fn main() {
-    let port = serialport::new(PORT_PATH, 115_200)
+    let mut port = serialport::new(PORT_PATH, BAUD)
         .timeout(Duration::from_secs(2))
         .open()
         .expect("USB device not found");
-    Ok(())
+    let mut row = 0;
+    loop {
+        _ = port.write(b"w\nc\n");
+        _ = port.write(&[b'r', row, b'\n']);
+        _ = port.write(&[b'd', 0b01000010, b'\n']);
+        row = (row + 1) % 8;
+        thread::sleep(Duration::from_secs(2));
+    }
 }
